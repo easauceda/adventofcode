bd8238909924c248d026608af9df0afcdff396ad
Erick Sauceda <easauceda@gmail.com>
2017-12-02 11:42:48 -0800

    Day One

---
+++ src/day_one.rs
@@ -0,0 +1,33 @@
+pub fn sum(input : String) -> u32{
+    let mut sum = 0;
+    // Convert the input string into a vector of chars
+    let chars : Vec<_> = input.chars().collect();
+    let step = chars.len() / 2;
+    
+    for i in 0..chars.len(){
+
+        let current = chars[i];
+        let next : char;
+
+        let mut next_char_pos = i + step;
+
+        debug!("Next Position: {}", next_char_pos);
+        
+        if next_char_pos > chars.len() - 1 {
+            // step should loop around to the correct index.
+            next_char_pos -= chars.len();
+            next = chars[next_char_pos];
+        } else {
+            next = chars[next_char_pos];
+        }
+        
+        if current == next {
+            debug!("Adding to total");
+            sum += current.to_digit(10).unwrap();
+        } else {
+            debug!("No Addition");
+        }
+        debug!("===========");
+    }
+    return sum;
+}
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@
 extern crate log;
 extern crate env_logger;
 
-
+mod day_one;
 mod util;
 
 fn main() {
     env_logger::init().unwrap();
 
     let input_one = util::get_input(1);
-
+    let result_one = day_one::sum(input_one);
+    println!("Day One Answer: {}", result_one);
 
 }
