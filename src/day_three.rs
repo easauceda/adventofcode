cc32903c0aa36a48984f3564973e33e162cee3b5
Erick Sauceda <easauceda@gmail.com>
2017-12-06 00:01:56 -0800

    implemented spiral algorithm

--- src/day_three.rs
+++ src/day_three.rs
@@ -1,7 +1,13 @@
 pub fn solve(input: String) -> (i32,i32) {
-    let coordinates = get_coordinates(input.parse::<i32>().unwrap());
+    let input_int = input.parse::<i32>().unwrap();
+
+    let coordinates = get_coordinates(input_int);
     let first_answer = coordinates.0.abs() + coordinates.1.abs();
-    return (first_answer, 0);
+
+    let second_answer = get_next_value(input_int);
+
+
+    return (first_answer, second_answer);
 }
 
 fn get_coordinates(target: i32) -> (i32, i32){
@@ -41,4 +47,53 @@ fn get_coordinates(target: i32) -> (i32, i32){
 
     debug!("{}, {}", x, y);
     return (x, y);
+}
+
+fn get_next_value(input: i32) -> i32 {
+    let mut row_index = 1;
+    let mut coordinates = (1,0);
+    // Use coordinates to determine neighbors
+    // TODO: Solve how to make turns
+
+    // if x = y: decrease y for turn
+    // if x = -row_index && y = row_index: move down; decrease x only
+    // if x = -row_index && y = -row_index: increase y
+    // if x = row_index && y = -row_index: row done
+    while row_index < 3 {
+
+        while coordinates != (row_index, row_index) {
+            coordinates = (coordinates.0, coordinates.1 + 1);
+
+            debug!("{},{}", coordinates.0, coordinates.1);
+        }
+
+        while coordinates != (-row_index, row_index) {
+            coordinates = (coordinates.0 - 1, coordinates.1);
+
+            debug!("Moving left");
+            debug!("{},{}", coordinates.0, coordinates.1);
+        }
+
+        while coordinates != (-row_index, -row_index) {
+            coordinates = (coordinates.0, coordinates.1 - 1);
+
+            debug!("Moving down");
+            debug!("{},{}", coordinates.0, coordinates.1);
+        }
+
+        while coordinates != (row_index, -row_index) {
+            coordinates = (coordinates.0 + 1, coordinates.1);
+
+            debug!("Moving right");
+            debug!("{},{}", coordinates.0, coordinates.1);
+        }
+
+        row_index += 1;
+        coordinates = (coordinates.0 + 1, coordinates.1);
+    }
+    unimplemented!()
+}
+
+fn find_sum(coordinates: (i32, i32)) -> i32 {
+    unimplemented!()
 }
