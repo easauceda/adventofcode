71732177b4ccf85653b10b07baac5c48a8b2c9a6
Erick Sauceda <easauceda@gmail.com>
2018-01-15 23:00:45 -0800

    Add boilerplate for string scoring helper function

--- src/day/four.rs
+++ src/day/four.rs
@@ -11,13 +11,14 @@ pub fn solve() -> (i32, i32){
     // if we get to the end, increment
     // return final count
     let mut count = 0;
+    let mut second_count = 0;
     let input = get_input();
 
     for line in input.lines(){
         let mut valid = true;
+        let mut more_valid = true;
         let mut row : HashSet<&str> = HashSet::new();
-
-
+        let mut unique_word_val : HashSet<i32> = HashSet::new();
 
         let words = line.as_ref().unwrap().split_whitespace();
 
@@ -27,14 +28,24 @@ pub fn solve() -> (i32, i32){
                 break;
             } else {
                 row.insert(&word);
+                let word_val = numerical_val(&word);
+                if unique_word_val.contains(&word_val){
+                    more_valid = false;
+                } else {
+                    unique_word_val.insert(word_val);
+                }
             }
         }
 
         if valid {
             count += 1;
+            if more_valid {
+                second_count += 1;
+            }
         }
     }
-    return (count,0);
+
+    return (count, second_count);
 }
 
 fn get_input() -> BufReader<File> {
@@ -42,4 +53,10 @@ fn get_input() -> BufReader<File> {
     let f = File::open(path).expect("Failed to open file");
 
     return BufReader::new(f);
+}
+
+fn numerical_val(input: &str) -> i32 {
+    // map each letter in the string to a numerical value, from 1 - 26
+    // return the sum of the map function
+    return 0;
 }
