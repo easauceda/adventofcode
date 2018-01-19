e6aa9cf5654fd73a962e0d49acb1294345789b57
Erick Sauceda <easauceda@gmail.com>
2018-01-18 19:59:36 -0800

    day 4 solution

--- src/day/four.rs
+++ src/day/four.rs
@@ -1,51 +1,28 @@
-use std::collections::HashSet;
+use itertools::Itertools;
+use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
-use std::fs::File;
 
 pub fn solve() -> (i32, i32){
-    // Read in
-    // for each line create hashset
-    // check for existence
-    // if exists, don't increment
-    // if we get to the end, increment
-    // return final count
-    let mut count = 0;
-    let mut second_count = 0;
     let input = get_input();
 
-    for line in input.lines(){
-        let mut valid = true;
-        let mut more_valid = true;
-        let mut row : HashSet<&str> = HashSet::new();
-        let mut unique_word_val : HashSet<i32> = HashSet::new();
+    let phrases : Vec<Vec<_>> = input.lines()
+		.filter_map(|line| line.ok())
+		.map(|line| line.split_whitespace().map(|w| w.to_string()).collect())
+        .collect();
 
-        let words = line.as_ref().unwrap().split_whitespace();
+    let valid_count = phrases.iter()
+        .filter(|phrase| phrase.iter().unique().count() == phrase.len())
+        .count() as i32;
 
-        for word in words {
-            if row.contains(&word){
-                debug!("{} is already in this phrase", word);
-                break;
-            } else {
-                row.insert(&word);
-                let word_val = numerical_val(&word);
-                if unique_word_val.contains(&word_val){
-                    more_valid = false;
-                } else {
-                    unique_word_val.insert(word_val);
-                }
-            }
-        }
+    let strict_valid_count = phrases.iter()
+        .filter(|phrase| {
+            let words : Vec<_> = phrase.iter().map(|w| w.chars().sorted()).collect();
+            words.iter().unique().count() == words.len()
+        })
+        .count() as i32;
 
-        if valid {
-            count += 1;
-            if more_valid {
-                second_count += 1;
-            }
-        }
-    }
-
-    return (count, second_count);
+    return (valid_count, strict_valid_count);
 }
 
 fn get_input() -> BufReader<File> {
@@ -53,10 +30,4 @@ fn get_input() -> BufReader<File> {
     let f = File::open(path).expect("Failed to open file");
 
     return BufReader::new(f);
-}
-
-fn numerical_val(input: &str) -> i32 {
-    // map each letter in the string to a numerical value, from 1 - 26
-    // return the sum of the map function
-    return 0;
 }
